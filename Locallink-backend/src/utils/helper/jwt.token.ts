57c11cf04acacc795bcb0c7474205dfe28d9ecac
saugat1070 <saugatgiri1070@gmail.com>
2026-08-02 03:04:29 +0545

    Feat: implement JWT token generation and response helper update

---
+++ Locallink-backend/src/utils/helper/jwt.token.ts
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import { Env } from "../../config/env.config.js";
+import { RefreshModel } from "../../models/refresh.model.js";
+
+export interface TokenPayload {
+    id: string | mongoose.Types.ObjectId;
+    type: "access" | "refresh";
+    refreshId?: string | mongoose.Types.ObjectId;
+    data?: any;
+}
+
+export const jwtToken = {
+    // refresh token for long term login
+    async refreshToken(payload: TokenPayload): Promise<{ token: string; refreshTokenId: string | mongoose.Types.ObjectId }> {
+        const token = jwt.sign(payload, Env.JWT_SECRET_REFRESH_TOKEN, { expiresIn: "15d" });
+        const refreshToken = await RefreshModel.create({
+            userId: payload.id,
+            refreshToken: token,
+        });
+        return { token, refreshTokenId: refreshToken._id };
+    },
+    // access token for short term login
+    accessToken(payload: TokenPayload): string {
+        return jwt.sign(payload, Env.JWT_SECRET_ACCESS_TOKEN, { expiresIn: "10m" });
+    },
+};
