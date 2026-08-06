4e0a48ec78bd428ea79dcd025b5a97b49601c697
saugat1070 <saugatgiri1070@gmail.com>
2026-08-07 00:20:55 +0545

    Feat: Refactor join_room and get_online_users socket events for improved user management

--- Locallink-backend/src/utils/socket/index.ts
+++ Locallink-backend/src/utils/socket/index.ts
@@ -2,6 +2,7 @@ import { Server } from "socket.io";
 import { redisClient } from "../../config/redis.config.js";
 import { UserService } from "../../modules/User/User.service.js";
 import { SocketPayLoad } from "../../middleware/socket.middleware.js";
+import { listOnlineUsers, markSocketOffline } from "./presence.socket.js";
 
 const userService = new UserService();
 const mapSocketId = (userId: string) => {
@@ -18,32 +19,23 @@ const redisSocketKey = (roomId: string, socketId: string) => {
 
 export const socketMethod = (io: Server) => {
     io.on("connection", (socket) => {
-        // on join room
-        socket.on("join_room", async (data: { roomId: string }) => {
-            if (socket.rooms.has(mapRoomId(data.roomId))) {
-                return {
-                    ack: true,
-                    message: "You are already in the room",
-                };
-            }
-            // join socket in room
-            socket.join(`room_${data.roomId}`);
-            // storing socket id in redis for online status
-            await redisClient.sadd(redisSocketKey(data.roomId, socket?.data?._id), socket.id);
-            // save socket room
-            await userService.joinSocketRoom(socket.data._id, data.roomId);
-
-            // Only the first socket for a user triggers "online"; only the last one leaving triggers "offline". Otherwise closing one of three tabs marks you offline for everyone.
-            const isFirstDevice = (await redisClient.scard(redisSocketKey(data.roomId, socket?.data?._id))) === 1;
-            if (isFirstDevice) {
-                socket.to(`room_${data?.roomId}`).emit("presence:changed", { roomId: data?.roomId, userId: socket.data?._id, status: "online" });
-            }
+        socket.on("join_room", async () => {
+            const myRooms = await userService.getUserRooms(socket.data._id); // fetching user's rooms
+            myRooms?.forEach((room) => socket.join(mapRoomId(room.roomId)));
         });
 
-        // on disconnecting
+        socket.on("get_online_users", async (data: { userIds: string[] }, callback) => {
+            const onlineUsers = await listOnlineUsers();
+            // storing online users in set to check if user is online or not
+            const onlineSet = new Set(onlineUsers);
+            callback(data.userIds.map((id) => ({ userId: id, isOnline: onlineSet.has(id) })));
+        });
+
         socket.on("disconnect", async () => {
-            // remove socket id from redis
-            await redisClient.srem(redisSocketKey(socket?.data?.roomId, socket?.data?._id), socket.id);
+            const wentOffline = await markSocketOffline(socket.data._id, socket.id);
+            if (wentOffline) {
+                io.emit("presence:changed", { userId: socket.data._id, isOnline: false });
+            }
         });
     });
 };
